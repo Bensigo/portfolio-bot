7b949ef6b1e0cfa5183d2ded9ca7170a5b17ecce
larry sigo <sigocode@gmail.com>
2017-07-17 13:48:51 +0100

    fixing error in handlemessage

--- controllers/handleMessage.js
+++ controllers/handleMessage.js
@@ -9,17 +9,17 @@ const processMessage = require('../helpers/processMessage');
  * call a recivedMessage method which is imported from helper
  * send a status code of 200 an 
  */
+   
 module.exports = (req,res)=>{
     const data = req.body
     if(data.object === 'page'){
-        data.entry.foreach(entry => { 
+       data.entry.foreach( entry => {
            entry.messaging.foreach(event=>{
-                        if(event.message){
-                            processMessage(event)  
-                        }
+               if(event.message){
+                processMessage(event)  
+               }
            })
-                
-           })
+       })
         res.status(200).end();
     }
     
